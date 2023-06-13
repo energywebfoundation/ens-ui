9e97d495b858e946aade5deca943a07f0fc0ab98
nichonien <ashish.tripathi@energyweb.org>
2023-06-13 08:53:31 +0530

    fix: add interface ids

--- src/constants/interfaces.js
+++ src/constants/interfaces.js
@@ -3,5 +3,10 @@ export const interfaces = {
   permanentRegistrar: '0x018fac06',
   permanentRegistrarWithConfig: '0xca27ac4c',
   baseRegistrar: '0x6ccb2df4',
-  dnsRegistrar: '0x1aa2e641'
+  dnsRegistrar: '0x1aa2e641',
+  bulkRenewal: '0x3150bfba',
+  dnssecClaimOld:'0x1aa2e641',
+  dnssecClaimNew:'0x17d8f49b',
+  // new interface id https://docs.ens.domains/ens-improvement-proposals/ensip-10-wildcard-resolution#specification
+  resolve:'0x9061b923'
 }
